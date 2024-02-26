3728defe1b901c593f37c4f6c80d514a175cdb91
dariom25 <dario@maciey.de>
2024-02-26 16:02:16 +0100

    Add surrounding div

--- src/components/LandingPage/landingPage.jsx
+++ src/components/LandingPage/landingPage.jsx
@@ -4,10 +4,10 @@ import { Headline } from "../Headline/headline";
 
 function LandingPage() {
   return (
-    <>
+    <div className="landing-page">
       <Headline text="+++ Create your own custom CV for FREE! +++" />
-      <Button text="Create a new CV" onClick={() => console.log("hello")} />
-    </>
+      <Button text="Create a new CV now" onClick={() => console.log("hello")} />
+    </div>
   );
 }
 
