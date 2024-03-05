a3046891cc867c4709a13aa8fa95618e5b79fbad
dariom25 <dario@maciey.de>
2024-03-05 17:45:09 +0100

    Overhaul the input component

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -1,5 +1,10 @@
-export default function Input() {
-    return(
-        <input/>
-    )
+function Input({ type="text", placeholder, labelText, inputDescription }) {
+  return (
+    <div className={`input-container ${inputDescription}`}>
+      <label>{labelText}</label>
+      <input type={type} placeholder={placeholder} />
+    </div>
+  );
 }
+
+export { Input };
