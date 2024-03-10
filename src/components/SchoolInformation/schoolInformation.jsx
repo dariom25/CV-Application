750a0f80b0d5fd3b4539da8dcd875977944d91cb
dariom25 <dario@maciey.de>
2024-03-10 13:10:09 +0100

    Change the key and id to the same number

--- src/components/SchoolInformation/schoolInformation.jsx
+++ src/components/SchoolInformation/schoolInformation.jsx
@@ -1,21 +1,28 @@
 import "./schoolInformation.css";
 import { Input } from "../Input/input";
+import { Button } from "../Button/button";
 
-function SchoolInformation() {
+function SchoolInformation({id}) {
+  const handleRemoveNumberEducationalExperience = (e) => {
+    e.preventDefault()
+    console.log(e.target.id)
+  }
+  
   return (
-    <div className="school-information">
+    <div className="school-information" key={id} id={id}>
       <Input
         placeholder={"ABC School"}
         labelText={"School/University/Vocational Service: "}
         inputDescription={"school"}
       />
       <Input
-        placeholder={"e.g. "}
+        placeholder={"e.g. College degree"}
         labelText={"Title of Study: "}
         inputDescription={"study-title"}
       />
       <Input type="date" labelText={"From: "} inputDescription={"school-from"} />
       <Input type="date" labelText={"To: "} inputDescription={"school-to"} />
+      <Button text={"X"} size="small" onClick={handleRemoveNumberEducationalExperience}/>
     </div>
   );
 }
