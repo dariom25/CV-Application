f43fd2dc90d13b89c55e7920d1861e2c83d08f94
dariom25 <dario@maciey.de>
2024-03-10 14:21:28 +0100

    Add button and func to remove practical experience

--- src/components/PracticalExperience/practicalExperience.jsx
+++ src/components/PracticalExperience/practicalExperience.jsx
@@ -1,9 +1,15 @@
 import "./practicalExperience.css";
 import { Input } from "../Input/input";
+import { Button } from "../Button/button";
 
-function PracticalExperience({key}) {
+function PracticalExperience({id, handleRemove}) {
+  const removePracticalExperience = (e) => {
+    e.preventDefault();
+    handleRemove(id)
+  }
+  
   return (
-    <div className="practical-information" key={key} id={key}>
+    <div className="practical-information" key={id} id={id}>
       <Input
         placeholder={"Example Comany SE"}
         labelText={"Company Name: "}
@@ -25,6 +31,7 @@ function PracticalExperience({key}) {
         inputDescription={"practical-from"}
       />
       <Input type="date" labelText={"To: "} inputDescription={"practical-to"} />
+      <Button text={"X"} size="small" onClick={removePracticalExperience}/>
     </div>
   );
 }
